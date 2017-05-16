091e07843776fe3d84fb54063ce3b801ae134bac
ranter132 <caiosouza1106@hotmail.com>
2017-05-16 09:29:50 -0300

    Update menu3.sh

--- menu3.sh
+++ menu3.sh
@@ -228,10 +228,101 @@ function GERD(){
 	dialog --title  "Construção." --msgbox "Estamos em construção" 0 0
 	MENU
 }
-function GDIS(){
-	clear
-	dialog --title "Construção." --msgbox "Estamos em construção" 0 0
-	MENU
+CTEC(){
+	dialog --yesno 'Aperte YES para configurar o teclado' 0 0
+	if (( $? == "0" )); then
+		sed 's/us/br/g' /etc/default/keyboard > /etc/default/keyboard.tmp && mv /etc/default/keyboard.tmp /etc/default/keyboard
+	else
+		GDIS
+	fi
+		if (( $? == '0' )); then
+			dialog --yesno 'Deseja reiniciar a máquina?' 0 0
+				if (( $? == '0' )); then
+					init 6
+				else
+					GDIS
+				fi
+		fi
+GDIS
+}
+IBAT(){
+clear
+acpi -V
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+ICPU(){
+clear
+lscpu
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+IMEM(){
+clear
+free -h
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+VDSO(){
+clear
+lsb_release -a
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+VKER(){
+clear
+uname -r
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+IPVI(){
+clear
+lspci | grep -i vga
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+TPCL(){
+clear
+uptime
+echo 'Digite [enter] para voltar'
+read xxx
+GDIS
+}
+GDIS(){
+	OPCAO=$(dialog						\
+		--stdout					\
+		--title 'Gerenciar Dispositivo'			\
+		--menu 'Escolha uma opção'			\
+		0 0 0						\
+		1 'Configurar Teclado'				\
+		2 'Informações da Bateria'			\
+		3 'Informações da CPU'				\
+		4 'Informações da Memória'			\
+		5 'Versão do S.O'				\
+		6 'Versão do kernel'				\
+		7 'Informação da Placa de Vídeo'		\
+		8 'Quanto tempo o PC está ligado'		\
+		9 'Voltar')
+
+	case $OPCAO in
+		1) CTEC ;;
+		2) IBAT ;;
+		3) ICPU ;;
+		4) IMEM ;;
+		5) VDSO ;;
+		6) VKER ;;
+		7) IPVI ;;
+		8) TPCL ;;
+		9) MENU ;;
+		255) exit 0 ;;
+		*) exit 0 ;;
+	esac
 }
 function CCGZIP(){
 	clear
