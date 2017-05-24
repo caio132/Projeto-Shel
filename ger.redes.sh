5b6ca94a6e55a84ec7212dee63e87a35d88cb433
ranter132 <caiosouza1106@hotmail.com>
2017-05-24 17:44:25 -0300

    Update ger.redes.sh

--- ger.redes.sh
+++ ger.redes.sh
@@ -8,37 +8,37 @@ OPCAO=$(dialog				\
 	1 'Gerenciar Arquivos'		\
 	2 'Sair'			)
 	case $OPCAO in
-		1) GARQ ;;
+		1) GRES ;;
 		2) FIM ;;
 		*) exit 0
 	esac
 }
-function GARQ(){
-OPCAO=$(dialog				\
-	--stdout			\
-	--title "MENU"			\
-	--menu "Escolha uma opção"	\
-	0 0 0 				\
-	1 "Criar Arquivo"		\
-	2 "Apagar Arquivo"		\
-	3 "Criar Diretorio"		\
-	4 "Apagar Diretorio"		\
-	5 "Mover Arquivo/Diretorio"	\
-	6 "Copiar Arquivo"		\
-	7 "Listar Arquivo"		\
-	8 "Permicionamento"		\
+function GRES(){
+OPCAO=$(dialog					\
+	--stdout				\
+	--title "MENU"				\
+	--menu "Escolha uma opção"		\
+	0 0 0 					\
+	1 "Listar redes"			\
+	2 "Listar rede especifica"		\
+	3 "Configurar rede manualmente"		\
+	4 "Desativar placa de rede"		\
+	5 "Ativar placa de rede"		\
+	6 "Visualizar redes fisicas "		\
+	7 "Mudar MAC"				\
+	8 "Reiniciar Sistema"			\
 	9 "Sair")
 
 	case $OPCAO in
 
-		1) CARQ ;;
-		2) AARQ ;;
-		3) CDIR ;;
-		4) ADIR ;;
-		5) MARQ ;;
-		6) CARQ ;;
-		7) LARQ ;;
-		8) PERM ;;
+		1) LRES ;;
+		2) LREES ;;
+		3) CRMA ;;
+		4) DPRE ;;
+		5) APRE ;;
+		6) VRFI ;;
+		7) MMAC ;;
+		8) RSIS ;;
 		9) FIM ;;
 		*) dialog --title "Opção Invalida" --msgbox "Digite Novamente" 0 0 ; MENU ;;
 	esac
@@ -50,60 +50,60 @@ FIM(){
 }
 
 GARQ=$(){
-function CARQ(){
+function LRES(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 > $NOME
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function AARQ(){
+function LREES(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 rm -r $NOME
 dialog --title "Informação" --msgbox "Arquivo apagado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function CDIR(){
+function CRMA(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do diretorio" 0 0)
 mkdir $NOME
 dialog --title "Informação" --msgbox "Diretorio criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function ADIR(){
+function DPRE(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 rmdir $NOME
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function MARQ(){
+function APRE(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 mv $NOME $ 
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function CARQ(){
+function VRFI(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 cp $NOME $
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function LARQ(){
+function MMAC(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 ls -la $NOME
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 
-function PERM(){
+function RSIS(){
 NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
 chmod +x $NOME
 dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GARQ
+GRES
 }
 while [[ $TEMP != 0 ]]; do
 	MENU || FIM
