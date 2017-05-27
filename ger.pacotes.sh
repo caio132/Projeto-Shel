062810c9daab699d0ada8fcc033e25dffdbf4b21
ranter132 <caiosouza1106@hotmail.com>
2017-05-27 03:03:28 -0300

    Update ger.pacotes.sh

--- ger.pacotes.sh
+++ ger.pacotes.sh
@@ -16,122 +16,60 @@ OPCAO=$(dialog				\
 		*) exit 0
 	esac
 }
-
-GREP(){
-OPCAO=$(dialog						\
-	--stdout					\
-	--title "MENU"					\
-	--menu "Escolha uma opção"			\
-	0 0 0 						\
-	1 "Compactar com o Gzip"			\
-	2 "Compactar com Gzip2"				\
-	3 "Instalar pacote"				\
-	4 "Desinstalar pacote"				\
-	5 "Atualizar repositorio"			\
-	6 "Listar repositorio"				\
-	7 "Compilar arquivos com .TAR"			\
-	8 "Compilar arquivos com .TAR.GZ"		\
-	9 "Compilar arquivos com .TAR.BZ2"		\
-       10 "Descompilar arquivos com .TAR"		\
-       11 "Descompilar arquivos com .TAR.GZ"		\
-       12 "Descompilar arquivos com .TAR.BZ2"		\
-       13 "Sair" )					\
-
-	case $OPCAO in
-
-		1) CGZPI   ;;
-		2) CGZIP2  ;;
-		3) IPAC    ;;
-		4) DPAC    ;;
-		5) AREP    ;;
-		6) LREP    ;;
-		7) CTAR    ;;
-		8) CTARGZ  ;;
-		9) CTARBZ2 ;;
-	       10) DTAR    ;;
-	       11) DTARGZ  ;;
-               12) DTARBZ2 ;;
-	       13) FIM
-
-	esac
-}
-FIM(){
-	dialog --title "FIM" --msgbox "Ate breve" 0 0
-	clear
-	exit 0
-}
-
-function CGZPI(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-> $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
-}
-function CGZIP2(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-rm -r $NOME
-dialog --title "Informação" --msgbox "Arquivo apagado com Sucesso" 0 0
-GREP
-}
 function IPAC(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do diretorio" 0 0)
-mkdir $NOME
-dialog --title "Informação" --msgbox "Diretorio criado com Sucesso" 0 0
+NOME=$(dialog --stdout --title "Nome" --inputbox "Digite nome do pacote a ser instalado" 0 0)
+apt-get install $NOME
+dialog --title "Informação" --msgbox "Pacote instalado com sucesso" 0 0
 GREP
 }
 function DPAC(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-rmdir $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
+NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do pacote a ser desinstalado" 0 0)
+apt-get remove $NOME
+if [[ $? == "0" ]]; then
+	dialog --title "Informação" --msgbox "Pacote desinstalado com sucesso" 0 0
+   else
+   	dialog --title "Informação" --msgbox " Pacote não encontrado" 0 0
+fi
 GREP
 }
 function AREP(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-mv $NOME $ 
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
+apt-get dist-upgrade 
+dialog --title "Informação" --msgbox "Sistema atualizado com sucesso" 0 0
 GREP
 }
-function LREP(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-cp $NOME $
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
+function LSPA(){
+apt list --installed | cat -n /tmp/lispar.txt > /tmp/lispar.txt
+dialog --textbox /tmp/lispar.txt 0 0
 GREP
 }
-function CTAR(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-ls -la $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
+function APA(){
+apt-get update 
+dialog --title "Informação" --msgbox "Atualização Completa" 0 0
 GREP
 }
-function CTARGZ(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-chmod +x $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
-}
-function CTARBZ2 (){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-chmod +x $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
-}
-function DTAR(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-chmod +x $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
-}
-function DTARGZ(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-chmod +x $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
-}
-function DTARBZ2(){
-NOME=$(dialog --stdout --title "Nome" --inputbox "Digite o nome do arquivo" 0 0)
-chmod +x $NOME
-dialog --title "Informação" --msgbox "Arquivo criado com Sucesso" 0 0
-GREP
+GREP(){
+OPCAO=$(dialog						\
+	--stdout 					\
+	--title "MENU"					\
+	--menu "Escolha uma opção"			\
+	0 0 0						\
+	1 "Instalar pacote"				\
+	2 "Desinstalar pacote"				\
+	3 "Atualizar sistema" 				\
+	4 "Listar pacotes"				\
+	5 "Atualizar pacotes"				\
+	6 "Voltar")
+
+	case $OPCAO in
+
+		1) IPAC    ;;
+		2) DPAC	   ;;
+		3) AREP    ;;
+		4) LSPA    ;;
+		5) APA     ;;
+	        6) MENU    ;;
+		*) dialog --title "Opção Invalida" --msgbox "Digite Novamente" 0 0 ; GREP ;;
+	esac
 }
 
 while [[ $TEMP != 0 ]]; do
